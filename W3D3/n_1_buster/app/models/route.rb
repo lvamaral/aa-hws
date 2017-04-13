7537874f3f437c19ec7c23e8b2ef411b060f82e0
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-04-12 20:46:07 -0700

    Complete N+1 Buster homework

--- W3D3/n_1_buster/app/models/route.rb
+++ W3D3/n_1_buster/app/models/route.rb
@@ -22,6 +22,19 @@ class Route < ActiveRecord::Base
   end
 
   def better_drivers_query
-    # TODO: your code here
+    buses = self.buses.includes(:drivers)
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      # will not fire a query for each route since drivers have already been prefetched
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
   end
+  
 end
