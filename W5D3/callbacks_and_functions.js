ea98965f0c6eb7dec1ca676419771383d41d2765
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-04-26 22:58:46 -0700

    Complete function calls exercises

--- W5D3/callbacks_and_functions.js
+++ W5D3/callbacks_and_functions.js
@@ -31,3 +31,26 @@ function teaAndBiscuits () {
     });
   });
 }
+
+function Cat () {
+  this.name = 'Markov';
+  this.age = 3;
+}
+
+function Dog () {
+  this.name = 'Noodles';
+  this.age = 4;
+}
+
+Dog.prototype.chase = function (cat) {
+  console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`)
+};
+
+const Markov = new Cat ();
+const Noodles = new Dog ();
+
+// Method style
+
+Noodles.chase(Markov);
+
+Noodles.chase.call(Markov, Noodles);
