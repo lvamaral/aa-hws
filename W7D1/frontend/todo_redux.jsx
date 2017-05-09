5dee4cc2db199cf437d89a1f21a638640e6819ad
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-05-08 23:19:00 -0700

    Create addLoggingToDispatch function

--- W7D1/frontend/todo_redux.jsx
+++ W7D1/frontend/todo_redux.jsx
@@ -4,6 +4,16 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+const addLoggingToDispatch = store => next => action => {
+  const originalDispatch = store.dispatch;
+  console.log(store.getState());
+  console.log(action);
+  let result = originalDispatch(action);
+  console.log(store.getState());
+  return result;
+};
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
     JSON.parse(localStorage.state) : {};
