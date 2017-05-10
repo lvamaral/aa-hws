3d8be6207bc0c7454a7559a2ddfedc0638f6f3bb
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-05-09 19:03:08 -0700

    Initial W7D2 homework commit

--- W7D1/frontend/todo_redux.jsx
+++ W7D1/frontend/todo_redux.jsx
@@ -4,6 +4,18 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+  const newStore = applyMiddlewares(store, addLoggingToDispatch);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
 const addLoggingToDispatch = store => next => action => {
   const originalDispatch = store.dispatch;
   console.log(store.getState());
@@ -13,12 +25,10 @@ const addLoggingToDispatch = store => next => action => {
   return result;
 };
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  const preloadedState = localStorage.state ?
-    JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
-
-  const root = document.getElementById('content');
-  ReactDOM.render(<Root store={store} />, root);
-});
+const applyMiddlewares = (store, ...middlewares) => {
+  let dispatch = store.dispatch;
+  middlewares.forEach((middleware) => {
+    dispatch = middleware(store)(dispatch);
+  });
+  return Object.assign({}, store, { dispatch });
+};
