20cfb881746e73a0ae793652cd65cc7e14204b7a
Lucas Amaral <lucas.v.amaral@gmail.com>
2017-07-03 22:12:59 -0700

    Remove W4D1 folder

--- W4D1/art_share_api/app/helpers/artworks_controller_helper.rb
+++
@@ -1,2 +0,0 @@
-module ArtworksControllerHelper
-end
--- W4D1/art_share_api/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  resources :users, except: [:new, :edit]
-  resources :artworks, except: [:new, :edit]
-  resources :artwork_shares, only: [:create, :destroy]
-end
